d04e4ff3a830612d72a6d2ea8db89614868d6808
Steven Hansen <steven.hansen@scalar.io>
2022-03-08 14:27:34 -0700

    Add force and remove branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,10 @@ set -ue
 
 RepositoryName="${INPUT_REPOSITORY_NAME}"
 AwsRegion="${INPUT_AWS_REGION}"
-Branch="${INPUT_BRANCH}"
 CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${RepositoryName}"
 
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
 git config --global push.default current
 git remote add sync ${CodeCommitUrl}
-git push sync 
+git push sync --force
