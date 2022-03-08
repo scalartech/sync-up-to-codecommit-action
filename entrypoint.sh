5f5897480296ffebd0b72e2bca8b10d8c54e4e81
Steven Hansen <steven.hansen@scalar.io>
2022-03-08 14:10:50 -0700

    Modify default push branch to current

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,6 @@ CodeCommitUrl="https://git-codecommit.${AwsRegion}.amazonaws.com/v1/repos/${Repo
 
 git config --global credential.'https://git-codecommit.*.amazonaws.com'.helper '!aws codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
+git config --global push.default current
 git remote add sync ${CodeCommitUrl}
 git push sync --mirror
